04703935ce38dad38e18c4fda00961336ba97ab9
ferdousr3 <ferdousr1992@gmail.com>
2025-12-08 12:00:33 +0600

    change url

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://managex-o.netlify.app'
 
 export const httpClient = axios.create({
    baseURL,
